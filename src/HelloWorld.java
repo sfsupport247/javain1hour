17d1589f11976f92caf3ef94c1468edbb3953f82
Sai Ram <contact@sfsupport247.com>
2024-03-20 15:33:52 -0400

    Added main()

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,7 @@
 public class HelloWorld {
-    
+    public static void main(String a[]) {
+        System.out.println("Hello World - This is our first Java/JDK 21 program");
+
+        System.out.println("Ran the first program with no Errors!");
+    }
 }
